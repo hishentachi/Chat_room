5399fe0bf3f700b078cbee21066839013c40a8e5
hishentachi <hishentachi@gmail.com>
2025-11-20 02:38:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -9,35 +9,53 @@ templates = Jinja2Templates(directory="templates")
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        # room_name -> list of WebSocket connections
+        self.rooms: dict[str, list[WebSocket]] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, room: str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if room not in self.rooms:
+            self.rooms[room] = []
+        self.rooms[room].append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        if websocket in self.active_connections:
-            self.active_connections.remove(websocket)
+    def disconnect(self, room: str, websocket: WebSocket):
+        if room in self.rooms:
+            if websocket in self.rooms[room]:
+                self.rooms[room].remove(websocket)
+            if not self.rooms[room]:
+                # remove empty room
+                del self.rooms[room]
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
+    async def broadcast(self, room: str, message: str):
+        if room not in self.rooms:
+            return
+        for connection in self.rooms[room]:
             await connection.send_text(message)
 
 
 manager = ConnectionManager()
 
 
-@app.get("/", response_class=HTMLResponse)
-async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/", include_in_schema=False)
+async def root():
+    # default room = lobby
+    return RedirectResponse(url="/room/lobby")
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
+@app.get("/room/{room_name}", response_class=HTMLResponse)
+async def get_room(request: Request, room_name: str):
+    # render same template with different room name
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "room_name": room_name}
+    )
+
+
+@app.websocket("/ws/{room_name}")
+async def websocket_endpoint(websocket: WebSocket, room_name: str):
+    await manager.connect(room_name, websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(data)
+            await manager.broadcast(room_name, data)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        manager.disconnect(room_name, websocket)
